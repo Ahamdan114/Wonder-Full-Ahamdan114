5364efe72d0015174e54d9dd3f6dddde7710daa8
Ahmed-Iulian Hamdan <Ahamdan114>
2024-01-25 17:54:31 +0200

    Updated profile

--- client/src/assets/profile-user.js
+++ client/src/assets/profile-user.js
@@ -1,8 +1,13 @@
 import { FiGithub } from "react-icons/fi";
 import { BsLinkedin } from "react-icons/bs";
 
+// Beginner Skills
+
 import { FaHtml5 } from "react-icons/fa6";
 import { IoLogoCss3 } from "react-icons/io";
+import { TbBrandJavascript } from "react-icons/tb";
+
+// Frameworks
 
 import { SiExpress } from "react-icons/si";
 import { GrReactjs } from "react-icons/gr";
@@ -10,23 +15,35 @@ import { GrReactjs } from "react-icons/gr";
 import { FaLocationDot } from "react-icons/fa6";
 import { IoMdMail } from "react-icons/io";
 
-import { SiSololearn } from "react-icons/si";
+// Tables
+
+import { DiMongodb } from "react-icons/di";
+import { TbSql } from "react-icons/tb";
 
 export const introductionPage = {
     tabIcon: "",
     title: "🚀 Fullstack Developer 🇷🇴",
     description:
-        "Fullstack developer in final year, skilled in React, Node.js, and MongoDB. Based in Bucharest, Romania. Passionate about crafting seamless web experiences.",
+        "Fullstack developer in final year, skilled in different technologies related to JavaScript, such as React.js, Express.js, Node.js, MongoDB and others. Based in Bucharest, Romania. Passionate about crafting seamless web experiences.",
     aboutMeTitle:
         "Passionate Fullstack Developer and life-long learner based in Bucharest, Romania | Hamdan Ahmed",
-    aboutMeDescription:
-        "Hello! I'm Hamdan Ahmed, a passionate and results-driven Fullstack Developer currently in my final year of studies. Based in Bucharest, Romania. I specialize in creating end-to-end web solutions that seamlessly blend functionality and aesthetics. I am a person of learning. One of my goals is to thrive in better understanding the world in which we are living in through numbers and other data types.",
+    aboutMeDescription: `
+        Hello! I'm Hamdan Ahmed, a final-year Fullstack Developer based in Bucharest, Romania. 
+        I specialize in developing web solutions that excel in both functionality and visual appeal, providing a polished user experience.`,
     socials: [FiGithub, BsLinkedin],
     socialsLinks: [
         "https://github.com/Ahamdan114/",
         "https://www.linkedin.com/in/ahmed-iulian-hamdan/",
     ],
-    skills: [FaHtml5, IoLogoCss3, SiExpress, GrReactjs],
+    skills: [
+        FaHtml5,
+        IoLogoCss3,
+        TbBrandJavascript,
+        SiExpress,
+        GrReactjs,
+        DiMongodb,
+        TbSql,
+    ],
     navBar: ["Home", "About Me", "Projects", "Contact Me"],
     contacts: [FaLocationDot, IoMdMail],
     location: "Bucharest, Romania",
@@ -55,6 +72,6 @@ export const introductionPage = {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrows: false
+        arrows: false,
     },
 };
