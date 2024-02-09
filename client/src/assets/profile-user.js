c1a2c1293b886878211192642d64c63a45cc3fc3
Ahmed-Iulian Hamdan <hamdan.ahmed002@gmail.com>
2024-02-09 09:23:51 +0200

    action: modified user profile

--- client/src/assets/profile-user.js
+++ client/src/assets/profile-user.js
@@ -25,7 +25,7 @@ export const introductionPage = {
     tabIcon: "",
     title: "🚀 Fullstack Developer 🇷🇴",
     description:
-        "Fullstack developer, skilled in different technologies related to JavaScript, such as React.js, Express.js, Node.js, MongoDB and others. Based in Bucharest, Romania. Passionate about crafting seamless web experiences.",
+        "Fullstack developer, skilled in different technologies related to JavaScript, such as React.js, Express.js, Node.js and others. Based in Bucharest, Romania. Passionate about crafting seamless web experiences.",
     aboutMeTitle:
         "Passionate Fullstack Developer and life-long learner based in Bucharest, Romania | Ahmed-Iulian Hamdan",
     aboutMeDescription: `
